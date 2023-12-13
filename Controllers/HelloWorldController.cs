7d0cc49892a7ea113cd5a30727f0de9fdb84c0e7
Yolisa.Myataza <yolisa.myataza@bsg.co.za>
2023-12-13 15:46:17 +0200

    Initial Commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("[controller]/")]
+        public async Task<string> CallApiAsync()
+        {
+            _logger.LogInformation("Hello World says the API");
+            return await Task.FromResult("Hello World !!");
+        }
+    }
+}
